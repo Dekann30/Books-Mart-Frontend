0378213ff44a6ec90da881fbaa03c74f31cbd9d2
Dominik Conway <dominik.conway@gmail.com>
2022-06-08 19:34:57 -0700

    Adds Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
