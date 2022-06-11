3a0f391fff835aa1b8b3ec6b489ca1bfb7e78e5f
Joe Park <sunghun.josephp@gmail.com>
2022-06-11 13:11:50 -0700

    Refactor functions in Books.jsx to be in App.js

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,44 +1,10 @@
-import { useEffect, useState } from "react"
-import Form from '../components/Form'
+import BookCard from '../components/BookCard'
 
 export default function Index() {
 
-  const [book, setBook] = useState(null)
-
-  const URL = "https://rj-books-mart-backend.herokuapp.com/books"
-
-  // index
-  const getBook = async () => {
-    const data = await fetch(URL).then(res => res.json())
-    setBook(data)
-    console.log(data)
-  }
-
-  // create
-  const createBook = async (book) => {
-    await fetch(URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "Application/json",
-      },
-      body: JSON.stringify(book)
-    })
-    getBook()
-  }
-
-  const updateBook = async (bookId) => {
-    // the rest of the function
-  }
-
-  const deleteBook = async (bookId) => {
-    // the rest of the function
-  }
-
-  useEffect(() => { getBook() }, [])
-
   return (
-    <section>
-      <Form createBook={createBook} />
-    </section>
+    <div>
+      <BookCard />
+    </div>
   )
 }
