36e39600fcc6780d432783d1f17077354e72c829
Joe Park <sunghun.josephp@gmail.com>
2022-06-11 17:58:50 -0700

    Style Books page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,9 @@ const StyledFooter = styled.footer`
   background: black;
   color: white;
   background-image: url(${props => props.bg});
+  min-height: 100px;
+  width: 100%;
+  margin-top: auto;
 `
 
 export default function Footer({ woodBg }) {
